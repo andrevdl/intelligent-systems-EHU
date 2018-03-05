c0d1515597d33bfa404b144ed89783e8de0ef607
andré van der Laan <andre.vd.laan@hotmail.com>
2018-03-05 15:44:33 +0000

    cleanup code, auto predict and export report

---
+++ intelligent Systems EHU/Classifier.py
@@ -0,0 +1,29 @@
+from sklearn.metrics import classification_report
+from sklearn.metrics import precision_recall_fscore_support
+import numpy as np
+from import_csv import load_data_from_csv
+
+class Classifier(object):
+    """description of class"""
+
+    def __init__(self, training, test):
+        self.training_feature_names, self.training_instances, self.training_labels = load_data_from_csv(input_csv=training)
+        self.test_feature_names, self.test_instances, self.test_labels = load_data_from_csv(input_csv=test)
+
+    def predict(self, classifier, export=True):
+        classifier.fit(self.training_instances, self.training_labels)
+        predicted_test_labels = classifier.predict(self.test_instances)
+        if export:
+            print(Classifier.average_report(self.test_labels, predicted_test_labels, digits=3))
+        else:
+            print(classification_report(self.test_labels, predicted_test_labels, digits=3))
+        
+    def average_report(y_true, y_pred, digits):
+        p, r, f1, s = precision_recall_fscore_support(y_true, y_pred)
+        # compute averages
+        return (u'{:>9.{digits}f}' * 3).format(np.average(p, weights=s), 
+                              np.average(r, weights=s),
+                              np.average(f1, weights=s),
+                              digits=digits)
+
+
--- intelligent Systems EHU/intelligent_Systems_ehu.py
+++ intelligent Systems EHU/intelligent_Systems_ehu.py
@@ -1,122 +1,76 @@
 from sklearn import tree
-from sklearn import svm
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.ensemble import AdaBoostClassifier
 from sklearn.neighbors import KNeighborsClassifier
-from sklearn.metrics import classification_report
 from sklearn.neural_network import MLPClassifier
-from sklearn.naive_bayes import MultinomialNB
 from mlxtend.classifier import StackingClassifier
 
 import numpy
 
 from multiprocessing import Pool
 
-import ensemble
-from import_csv import load_data_from_csv
+from Classifier import Classifier
 
 CONNECT4_TRAINING = 'data/connect4/connect-4.training.csv';
 CONNECT4_TEST = 'data/connect4/connect-4.test.csv';
 TIC_TRAINING = 'data/tic_tac_toe/tic-tac-toe.training.csv';
 TIC_TEST = 'data/tic_tac_toe/tic-tac-toe.test.csv';
 
-CURRENT_TRAINING = CONNECT4_TRAINING;
-CURRENT_TEST = CONNECT4_TEST;
+def execute(classifier, task=0):
+    if task == 0:
+        for x in range(1, 51):
+            cl.predict(KNeighborsClassifier(x, n_jobs=8))
+    elif task == 1:
+        cl.predict(tree.DecisionTreeClassifier())
+    elif task == 2:
+        for x in numpy.arange(10, 301, 10):
+            cl.predict(RandomForestClassifier(x, n_jobs=8, max_features='auto'))
+    elif task == 3:
+        cl.predict(MLPClassifier(hidden_layer_sizes=(1000,), max_iter=5000000, solver='lbfgs'))
+    elif task == 4:
+        cl.predict(MLPClassifier(hidden_layer_sizes=(1000,), max_iter=5000000, solver='adam'))
+    elif task == 5:
+        cl.predict(AdaBoostClassifier(base_estimator=RandomForestClassifier(40, n_jobs=8), n_estimators=5000))
 
-def execute_majority_vote(estimators):
-    ensemble.majority_vote(
-        CONNECT4_TRAINING,
-        CONNECT4_TEST,
-        estimators
-    )
+def stacking(classifier, optimalRF, optimalKNN, task=0):
+    if task == 0:
+        for x in range(1, 51):
+            cl.predict(StackingClassifier(
+                classifiers=[RandomForestClassifier(optimalRF, n_jobs=8), KNeighborsClassifier(optimalKNN)], 
+                meta_classifier=KNeighborsClassifier(x, n_jobs=8)))
+    elif task == 1:
+        for x in numpy.arange(10, 301, 10):
+            cl.predict(StackingClassifier(
+                classifiers=[RandomForestClassifier(optimalRF, n_jobs=8), KNeighborsClassifier(optimalKNN)], 
+                meta_classifier=RandomForestClassifier(x, n_jobs=8)))
+    elif task == 2:
+        cl.predict(StackingClassifier(
+            classifiers=[RandomForestClassifier(optimalRF, n_jobs=8), KNeighborsClassifier(optimalKNN)], 
+            meta_classifier=tree.DecisionTreeClassifier()))
 
-def execute_confidence_based(estimators):
-    ensemble.confidence_based_ensemble(
-        CONNECT4_TRAINING,
-        CONNECT4_TEST,
-        estimators
-    )
+def test_program(classifier):
+    print('K-NN')
+    execute(classifier, 0)
+    print('DecisionTreeClassifier')
+    execute(classifier, 1)
+    print('RandomForestClassifier')
+    execute(classifier, 2)
+    print('MLPClassifier (lbfgs)')
+    execute(classifier, 3)
+    print('MLPClassifier (adam)')
+    execute(classifier, 4)
+    print('AdaBoostClassifier')
+    execute(classifier, 5)
 
-def KNN(n):
-    training_feature_names, training_instances, training_labels = load_data_from_csv(input_csv=CURRENT_TRAINING)
-    test_feature_names, test_instances, test_labels = load_data_from_csv(input_csv=CURRENT_TEST)
-
-    classifier = KNeighborsClassifier(n_neighbors=n)
-    classifier.fit(training_instances, training_labels)
-    predicted_test_labels = classifier.predict(test_instances)
-
-    print("Neighbors = ", n)
-    print(classification_report(test_labels, predicted_test_labels, digits=3))
-
-def DecisionTree():
-    training_feature_names, training_instances, training_labels = load_data_from_csv(input_csv=CURRENT_TRAINING)
-    test_feature_names, test_instances, test_labels = load_data_from_csv(input_csv=CURRENT_TEST)
-
-    classifier = tree.DecisionTreeClassifier()
-    classifier.fit(training_instances, training_labels)
-    predicted_test_labels = classifier.predict(test_instances)
-
-    print(classification_report(test_labels, predicted_test_labels, digits=3))
-
-def RandomForest(e, f):
-    training_feature_names, training_instances, training_labels = load_data_from_csv(input_csv=CURRENT_TRAINING)
-    test_feature_names, test_instances, test_labels = load_data_from_csv(input_csv=CURRENT_TEST)
-
-    classifier = RandomForestClassifier(e, n_jobs=8, max_features=f)
-    classifier.fit(training_instances, training_labels)
-    predicted_test_labels = classifier.predict(test_instances)
-
-    print(classification_report(test_labels, predicted_test_labels, digits=3))
-
-def Neutral(solver):
-    training_feature_names, training_instances, training_labels = load_data_from_csv(input_csv=CURRENT_TRAINING)
-    test_feature_names, test_instances, test_labels = load_data_from_csv(input_csv=CURRENT_TEST)
-
-    classifier = MLPClassifier(hidden_layer_sizes=(1000,), max_iter=5000000, solver=solver);
-    classifier.fit(training_instances, training_labels)
-    predicted_test_labels = classifier.predict(test_instances)
-
-    print(classification_report(test_labels, predicted_test_labels, digits=3))
-
-def AdaBoost():
-    training_feature_names, training_instances, training_labels = load_data_from_csv(input_csv=CURRENT_TRAINING)
-    test_feature_names, test_instances, test_labels = load_data_from_csv(input_csv=CURRENT_TEST)
-
-    classifier = AdaBoostClassifier(base_estimator=RandomForestClassifier(40, n_jobs=8), n_estimators=5000)
-    classifier.fit(training_instances, training_labels)
-    predicted_test_labels = classifier.predict(test_instances)
-
-    print(classification_report(test_labels, predicted_test_labels, digits=3))
-
-def Stacking(classifiers, meta):
-    training_feature_names, training_instances, training_labels = load_data_from_csv(input_csv=CURRENT_TRAINING)
-    test_feature_names, test_instances, test_labels = load_data_from_csv(input_csv=CURRENT_TEST)
-
-    classifier = StackingClassifier(classifiers=classifiers, meta_classifier=meta)
-    classifier.fit(training_instances, training_labels)
-    predicted_test_labels = classifier.predict(test_instances)
-
-    print(classification_report(test_labels, predicted_test_labels, digits=3))
+def test_program_stacking(classifier, optimalRF, optimalKNN):
+    print('StackingClassifier (KNeighborsClassifier)')
+    stacking(classifier, optimalRF, optimalKNN, 0)
+    print('StackingClassifier (RandomForestClassifier)')
+    stacking(classifier, optimalRF, optimalKNN, 1)
+    print('StackingClassifier (DecisionTreeClassifier)')
+    stacking(classifier, optimalRF, optimalKNN, 2)
 
 if __name__ == '__main__':
-    #for x in range(25, 51): # 1 - 25
-    #    KNN(x)
-
-    #DecisionTree();
-
-    #for x in numpy.arange(10, 301, 10):
-    #    RandomForest(x, 'auto')
-
-    Neutral('lbfgs');
-    #Neutral('adam');
-
-    #AdaBoost();
-
-    #Stacking([RandomForestClassifier(110, n_jobs=8), KNeighborsClassifier(5)], KNeighborsClassifier(1))
-    #Stacking([RandomForestClassifier(110, n_jobs=8), KNeighborsClassifier(5)], RandomForestClassifier(110, n_jobs=8))
-    #Stacking([RandomForestClassifier(110, n_jobs=8), KNeighborsClassifier(5)], tree.DecisionTreeClassifier())
-
-    # OLD STUFF
-    #p = Pool(5)
-    #p.map(KNN, range(1, 5))
-    #p.map(execute_majority_vote, [estimators, estimators, estimators, estimators, estimators, estimators])
+    cl = Classifier(CONNECT4_TRAINING, CONNECT4_TEST)
+    #test_program(cl)
+    execute(cl, 4)
